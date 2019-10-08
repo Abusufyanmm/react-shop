1914c6c5c805f35f60fa32de7824bbaec4d8c8c7
abusufyanmm <sufyanmaiwada@yahoo.com>
2019-10-08 10:06:53 +0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const storeProducts = [
+    {
+        id:1,
+        title: "Google pixel",
+        img: "img/img1.jpg",
+        price: 10,
+        company: "Google",
+        info: "tem ben rem sen rfcv gfgcgc fgds fsrsr sesesd rsss rsr sr  rrsrsrsr bygyg hgyg hygy hgygy yfyfy yfyfy yfyfy yfyfhvy",
+        inCart:false,
+        count:0,
+        total:0
+    },
+
+    {
+        id:2,
+        title: "Dell Book",
+        img: "img/img2.jpg",
+        price: 50,
+        company:"Dell",
+        info: "tem ben rem sen rfcv gfgcgc fgds fsrsr sesesd rsss rsr sr  rrsrsrsr bygyg hgyg hygy hgygy yfyfy yfyfy yfyfy yfyfhvy",
+        inCart:false,
+        count:0,
+        total:0
+    },
+
+    {
+        id:3,
+        title: "Sony Tv",
+        img: "img/img3.jpg",
+        price: 100,
+        company: "Sony",
+        info: "tem ben rem sen rfcv gfgcgc fgds fsrsr sesesd rsss rsr sr  rrsrsrsr bygyg hgyg hygy hgygy yfyfy yfyfy yfyfy yfyfhvy",
+        inCart:false,
+        count:0,
+        total:0
+    },
+
+    {
+        id:4,
+        title: "Asus Viva",
+        img: "img/img6.jpg",
+        price: 30,
+        company: "Asus",
+        info: "tem ben rem sen rfcv gfgcgc fgds fsrsr sesesd rsss rsr srbv ygyg  rrsrsrsr bygyg hgyg hygy hgygy yfyfy yfyfy yfyfy yfyfhvy",
+        inCart:false,
+        count:0,
+        total:0
+    },
+
+    {
+        id:5,
+        title: "Apple Mac",
+        img: "img/img5.jpg",
+        price: 10,
+        company: "Apple",
+        info: "tem ben rem sen rfcv gfgcgc fgds fsrsr sesesd rsss rsr sr  rrsrsrsr bygyg hgyg hygy hgygy yfyfy yfyfy yfyfy yfyfhvy",
+        inCart:false,
+        count:0,
+        total:0
+    }
+
+];
+
+export const detailProduct = {
+    id: 1,
+    title: "Google - Black",
+    img: "img/img1.jpg",
+    price: 10,
+    company: "google",
+    info:"gagags agag kaka tsre ystetgd sgsgs tew sshh fasd gads ggas yer rat yapn sgdysd dgdfss dsfdfs dfsdsfd d sd syycxv d fhvhuvuf ffvf vfy yv.",
+    inCart:false,
+    count: 0,
+    total: 0
+};
